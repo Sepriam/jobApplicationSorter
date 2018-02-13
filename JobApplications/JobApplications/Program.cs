30ef73e0f38dce7d3b3dcf7a79efc70ed4d359d7
Sepriam <34950923+Sepriam@users.noreply.github.com>
2018-02-13 21:42:29 +0000

    Add files via upload
    Initial Commit:
    Need to change access rights of List -- Too tired to remember how to store a list of objects without overriding said list of objects

---
+++ JobApplications/JobApplications/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplications
+{
+    class Program
+    {
+        public List<JobObject> listOfJobs = new List<JobObject>();
+
+
+
+        static void Main(string[] args)
+        {
+
+            startProgram();
+
+        }
+
+
+        static void startProgram()
+        {
+            Console.WriteLine("Hello, this program is an easy way to quick, store and display your current job applications");
+
+            DisplayInfo();
+
+            string command = Console.ReadLine();
+
+            decideAction(command);
+        }
+
+        static void DisplayInfo()
+        {
+           
+            Console.WriteLine("You can choose from any of the following options:");
+
+            Console.WriteLine("Info   - Displays info of a job or all jobs");
+            Console.WriteLine("Add    - Allows you to add a new job to system");
+            Console.WriteLine("Delete - Allows you to delete any of the jobs you've applied for");
+            Console.WriteLine("Exit   - Saves and exits the application");
+            Console.WriteLine("?      - Displays these options again");
+
+            Console.WriteLine("Please enter a command:");
+        }
+
+        static void decideAction(string _command)
+        {
+
+            JobObject jobs = new JobObject();
+
+            switch (_command)
+            {
+                case "Info":
+                    jobs.showJobs();
+                    break;
+                case "Add":
+                    jobs.Addjob();
+                    break;
+                case "Delete":
+                    jobs.DeleteJob();
+                    break;
+                case "Exit":
+                    break;
+                case "?":
+                    DisplayInfo();
+                    break;
+                default:
+                    Console.WriteLine("Incorrect Input");
+                    break;
+            }
+
+            Console.WriteLine("Please enter a command: ");
+
+            string command = Console.ReadLine();
+            decideAction(command);
+
+        }
+    }
+}
---
+++ JobApplications/JobApplications/jobObject.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplications
+{
+    class JobObject
+    {
+        Program pr = new Program();
+
+        private string Name, Company, Location, Salary, Hours;
+
+        public JobObject(string _name, string _company, string _location, string _salary, string _hours)
+        {
+            this.Name = _name;
+            this.Company = _company;
+            this.Location = _location;
+            this.Salary = _salary;
+            this.Hours = _hours;
+        }
+
+        public JobObject() { }
+        
+
+        public void Addjob()
+        {
+            Console.WriteLine("Please follow the directions as listed. If an error is made, type REDO to start again.");
+            Console.WriteLine("1) Please enter the job name:");
+            string jobName = Console.ReadLine();
+            if (jobName == "REDO")
+            {
+                Addjob();
+            }
+
+            Console.WriteLine("2) Please enter the Company name: ");
+            string companyName = Console.ReadLine();
+            if (companyName == "REDO")
+            {
+                Addjob();
+            }
+
+            Console.WriteLine("3) Please enter the Location: ");
+            string _location = Console.ReadLine();
+            if (_location == "REDO")
+            {
+                Addjob();
+            }
+
+            Console.WriteLine("4) Please enter the Salary: ");
+            string salaryString = Console.ReadLine();
+            if (salaryString == "REDO")
+            {
+                Addjob();
+            }
+
+            Console.WriteLine("5) Please enter the hours:");
+            string hoursString = Console.ReadLine();
+            if (hoursString == "REDO")
+            {
+                Addjob();
+            }
+
+            JobObject job = new JobObject(jobName, companyName, _location, salaryString, hoursString);
+
+            pr.listOfJobs.Add(job);
+            
+        }
+
+
+        public void DeleteJob()
+        {
+            Console.WriteLine("Current jobs on system:");
+            foreach (JobObject a in pr.listOfJobs)
+            {
+                Console.WriteLine("Job Name: " + a.Name + ", Company: " + a.Company);
+            }
+
+            Console.WriteLine("Please enter the name of the company who's job you wish to delete: ");
+
+            bool correctDelete = false;
+            while (correctDelete == false)
+            {
+                string deleteJob = Console.ReadLine();
+
+                foreach (JobObject a in pr.listOfJobs)
+                {
+                    if (deleteJob == a.Company)
+                    {
+                        pr.listOfJobs.Remove(a);
+                        Console.WriteLine("Job " + deleteJob + " has been removed");
+                        correctDelete = true;
+                    }
+                }
+            }
+        }
+
+
+        public void showJobs()
+        {
+            if (pr.listOfJobs.Count < 1)
+            {
+                Console.WriteLine("No Current Jobs in System");
+                return;
+            }
+            
+            Console.WriteLine("The current jobs on system are:");
+            foreach (JobObject a in pr.listOfJobs)
+            {
+                Console.WriteLine("Job Name: " + a.Name + ", Company: " + a.Company);
+            }
+
+            Console.WriteLine("Please enter the company who's job you want to know the info of: ");
+
+            bool correctInfo = false;
+            while (correctInfo == false)
+            {
+                string infoOfJob = Console.ReadLine();
+
+                foreach (JobObject a in pr.listOfJobs)
+                {
+                    if (infoOfJob == a.Company)
+                    {
+                        Console.Write("\nJob role: " + a.Name + "\nLocation: " + a.Location + "\nSalary: " + a.Salary + "\nHours: " + a.Hours);
+                        correctInfo = true;
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Please enter a valid company name: ");
+            }
+        }
+    }
+}
